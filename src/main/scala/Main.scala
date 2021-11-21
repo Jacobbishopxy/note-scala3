34ec7196740aba8f0f72d7f2096ae5fc6699fb75
Jacob <jacobbishopxy@gmail.com>
2021-11-21 14:17:12 +0800

    new 3. Control Structures.md

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
 @main def hello: Unit =
   println("Hello world!")
   println(msg)
+  println(day)
 
 def msg = "I was compiled by Scala 3. :)"
+
+val i = 2
+
+val day = i match
+  case 0 => "Sunday"
+  case 1 => "Monday"
+  case 2 => "Tuesday"
+  case 3 => "Wednesday"
+  case 4 => "Thursday"
+  case 5 => "Friday"
+  case 6 => "Saturday"
+  case _ => "invalid day"
